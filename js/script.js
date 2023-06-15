015aa5e3e2f41bbd05f90e7b3c6213d819f40aee
Liamb44 <liamrbrandt@gmail.com>
2023-06-15 11:55:31 -0400

    added random quote generator

--- js/script.js
+++ js/script.js
@@ -3,9 +3,18 @@
 //global scoped variables to contain the info being passed from the functions
 var userData;
 var workoutData;
-
+var baseURL = "https://type.fit/api/quotes";+
 
 // Scripts for pages
+
+
+
+//script for generating a random quote
+$.get(baseURL).then(function(data) {
+    var updatedData = JSON.parse(data);
+    var randomNum = Math.floor(Math.random() * updatedData.length);
+    console.log(updatedData[randomNum].text);
+  });
 
 function getUserInfoModal() {
     var userInfo = {
