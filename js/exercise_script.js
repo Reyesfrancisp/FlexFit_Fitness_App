36a4fd0db5e903e42e184e8191ca9fe01f863e37
Francis Reyes <reyesfrancisp@gmail.com>
2023-06-19 12:33:12 -0400

    Added working search info to display in the section

--- js/exercise_script.js
+++ js/exercise_script.js
@@ -1,6 +1,7 @@
 
 //global scoped variables
 var workoutData;
+var exerciseData = [];
 var dataOutput = document.getElementById('messageBody');
 // get user info from the modal form IDs
 
@@ -24,9 +25,7 @@ function getUserWorkoutInfo() {
     return workoutInfo; //return the object
 }
 
-
 //function to get excercises from API
-
 function getExercises() {
     var muscle = workoutData.muscle;
     var type = workoutData.type;
@@ -35,25 +34,44 @@ function getExercises() {
     var exerciseByMuscle = [];
     var finalExerciseList = [];
 
+    if (muscle == "random") {
+        var options = $("#muscleForm option").not(":first"); // Get all the option elements that isn't the first one
+        var randomIndex = Math.floor(Math.random() * options.length); // Generate a random index
+        var randomMuscle = options[randomIndex].value; // Get the value of the randomly selected option
+        muscle = randomMuscle;
+    }
+
     $.ajax({
         method: "GET",
         url: "https://api.api-ninjas.com/v1/exercises?muscle=" + muscle,
         headers: { "X-Api-Key": "uzXoqXKJQzocibLJBGGrFw==5EMXvqAyhQA7R36e" },
         contentType: "application/json",
-        success: function(result) {
+        success: function (result) {
             exerciseByMuscle = result;
             var count = 0;
             while (count < exerciseByMuscle.length) {
-                if (
-                    exerciseByMuscle[count].type == type &&
-                    exerciseByMuscle[count].difficulty == difficulty
-                ) {
+                if (exerciseByMuscle[count].type == type && exerciseByMuscle[count].difficulty == difficulty) {
                     finalExerciseList.push(exerciseByMuscle[count]);
                 }
+                else
+                    if (type == "random" && exerciseByMuscle[count].difficulty == difficulty) {
+                        finalExerciseList.push(exerciseByMuscle[count]);
+                    }
+                    else
+                        if
+                            (exerciseByMuscle[count].type == type && difficulty == "random") {
+                            finalExerciseList.push(exerciseByMuscle[count]);
+                        }
+                        else
+                            if
+                                (type == "random" && difficulty == "random") {
+                                finalExerciseList.push(exerciseByMuscle[count]);
+                            }
+
                 count++;
             }
             console.log("finalExerciseList below");
-            console.log(finalExerciseList);    
+            console.log(finalExerciseList);
             displayData(finalExerciseList);
 
         },
@@ -64,26 +82,44 @@ function getExercises() {
 
 }
 
-
 //initialize the event listeners
 function pageInitialize() {
 
     $("#confirmButton").click(function () {
-       console.log("Confirm Clicked");
+        console.log("Confirm Clicked");
         workoutData = getUserWorkoutInfo();
-        console.log(workoutData);        
+        console.log(workoutData);
         getExercises();
 
     });
+
+    // Event listener for button click
+    $("#messageBody").on("click", ".exerciseDisplayButton", function () {
+        var exerciseIndex = $(this).data("index");
+        var exerciseDataItem = exerciseData[exerciseIndex];
+        console.log(exerciseDataItem);
+        $("#messageBody").empty();
+        var execiseDetails = `<li> Name of the exercise: ${exerciseDataItem.name}</li>
+        <li> Muscle Group Targeted: ${exerciseDataItem.muscle}</li>
+        <li> Equipment Needed: ${exerciseDataItem.equipment}</li>
+        <li> Difficulty of the exercise: ${exerciseDataItem.difficulty}</li>
+        <li> Type of the exercise: ${exerciseDataItem.type}</li>
+        <li> Instructions: ${exerciseDataItem.instructions}</li>`;
+        $("#messageBody").append(execiseDetails);
+    });
+
 }
 
-
-
-function displayData(data)
-{
+function displayData(data) {
     console.log(data);
-    dataOutput.textContent = data[0].name;
+    exerciseData = data; // Assign the data to the exerciseData array
+    $("#messageBody").empty();
+    for (var index = 0; index < data.length; index++) {
+        var exerciseOutput = `<li><button type="button" class="exerciseDisplayButton" data-index="${index}">Workout: ${data[index].name}</button></li>`;
+        $("#messageBody").append(exerciseOutput);
+    }
 }
+
 
 
 //run after the document is ready
